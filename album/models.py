eea767e3309e84da664ebd11612bd48e74963248
warshavskiythebest <warshavskiythebest@gmail.com>
2017-11-29 12:42:45 +0200

    first commit

---
+++ album/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class PhotoModel(models.Model):
+    photo=models.FileField(upload_to='photos/')
+    file_name=models.TextField()
+    file_description=models.TextField()
+    file_date=models.DateTimeField(auto_now_add=True)
